1f0a65790b3c538514299dfa15ee6a2b902eef55
Mayya <khmelnitskaya@wiseadvice.ru>
2019-07-03 17:16:42 +0300

    h

--- LEGALSUP-2746_fix_financial_perfomance/js/main.js
+++ LEGALSUP-2746_fix_financial_perfomance/js/main.js
@@ -2471,20 +2471,6 @@ var VerticalTabs = {
 
 App.Control.install(VerticalTabs);
 
-var InfoSlider = {
-	el: '.js-info-slider',
-	name: 'InfoSlider',
-	initialize: function () {
-		this.$el.bxSlider({
-			mode: 'fade',
-			pager: false,
-			auto: false,
-			adaptiveHeight: true,
-		});
-	}
-};
-App.Control.install(InfoSlider);
-
 var ExpertsSlider = {
 	el: '.js-experts-slider',
 	name: 'ExpertsSlider',
@@ -2509,118 +2495,6 @@ var ExpertsSlider = {
 
 App.Control.install(ExpertsSlider);
 
-
-var InfoSliderLp = {
-	el: '.js-info-slider-lp',
-	name: 'InfoSliderLp',
-	slider: null,
-	initialize: function () {
-		this.slider = this.$el.bxSlider({
-			slideMargin: 20,
-			adaptiveHeight: false,
-			infiniteLoop: true
-		});
-	}
-
-
-};
-
-App.Control.install(InfoSliderLp);
-
-var MainNavView = {
-    el: '.js-main-nav',
-    name: 'MainNavView',
-    initialize: function() {
-        this.mainNavBtn = this.$('.js-main-nav__btn');
-        this.mainNavList = this.$('.js-main-nav__list');
-        this.mainNavOffsetTop = this.$el.offset().top;
-        this.mainNavHeight = this.$el.outerHeight();
-
-        var self = this;
-
-        $(window).bind('resize', function () {
-            self.mainNavOffsetTop = self.$el.offset().top;
-        });
-
-        $(window).bind('scroll', function () {
-            self.fixedNav();
-        });
-    },
-
-    events: {
-        'click .js-main-nav__btn': 'toggleNav'
-    },
-
-    toggleNav: function() {
-        this.mainNavList.toggleClass('main-nav__list--open')
-    },
-
-    fixedNav: function() {
-        if ( $(window).scrollTop() > this.mainNavOffsetTop) {
-            this.$el.addClass('main-nav--fixed');
-        } else {
-            this.$el.removeClass('main-nav--fixed');
-        }
-    }
-};
-
-App.Control.install(MainNavView);
-var MainSlider = {
-    el: '.js-main-slider',
-    name: 'MainSlider',
-    initialize: function() {
-        this.$el.bxSlider({
-            mode: 'fade',
-            pager: false,
-            auto: true,
-        });
-    }
-};
-
-App.Control.install(MainSlider);
-var VisitedPages = {
-	el: '.js-visited-pages',
-	name: 'VisitedPages',
-
-	initialize: function() {
-		this.mainSlider = $('.main-slider');
-		this.mainSliderOffsetTop = this.mainSlider.offset().top;
-		this.mainSliderHeight = this.mainSlider.outerHeight();
-
-		this.container = this.$el.parent('.container');
-		this.containerWidth = this.container.outerWidth();
-		this.elWidth = ($(window).width() - this.containerWidth) / 2;
-
-		this.pushPoint = this.mainSliderOffsetTop + this.mainSliderHeight;
-
-		var self = this;
-
-		this.setStickyBlockWidth();
-
-		$(window).bind('resize', function() {
-			self.elWidth = ($(window).width() - self.containerWidth) / 2;
-			self.setStickyBlockWidth();
-		});
-
-		$(window).bind('scroll', function() {
-			self.stickyOnScroll();
-		});
-	},
-
-	setStickyBlockWidth: function() {
-		this.$el.css({'width': this.elWidth});
-	},
-
-	stickyOnScroll: function() {
-		if($(window).scrollTop() >= this.pushPoint) {
-			this.$el.addClass('visited-pages--fixed');
-		} else {
-			this.$el.removeClass('visited-pages--fixed');
-		}
-	}
-};
-
-App.Control.install(VisitedPages);
 App.Control.install({
     el: '.input-checkbox',
     name: 'InputCheckbox',
@@ -3091,3 +2965,129 @@ App.Control.install({
             return $();
     }
 });
+var InfoSlider = {
+	el: '.js-info-slider',
+	name: 'InfoSlider',
+	initialize: function () {
+		this.$el.bxSlider({
+			mode: 'fade',
+			pager: false,
+			auto: false,
+			adaptiveHeight: true,
+		});
+	}
+};
+App.Control.install(InfoSlider);
+
+
+var InfoSliderLp = {
+	el: '.js-info-slider-lp',
+	name: 'InfoSliderLp',
+	slider: null,
+	initialize: function () {
+		this.slider = this.$el.bxSlider({
+			slideMargin: 20,
+			adaptiveHeight: false,
+			infiniteLoop: true
+		});
+	}
+
+
+};
+
+App.Control.install(InfoSliderLp);
+
+var MainNavView = {
+    el: '.js-main-nav',
+    name: 'MainNavView',
+    initialize: function() {
+        this.mainNavBtn = this.$('.js-main-nav__btn');
+        this.mainNavList = this.$('.js-main-nav__list');
+        this.mainNavOffsetTop = this.$el.offset().top;
+        this.mainNavHeight = this.$el.outerHeight();
+
+        var self = this;
+
+        $(window).bind('resize', function () {
+            self.mainNavOffsetTop = self.$el.offset().top;
+        });
+
+        $(window).bind('scroll', function () {
+            self.fixedNav();
+        });
+    },
+
+    events: {
+        'click .js-main-nav__btn': 'toggleNav'
+    },
+
+    toggleNav: function() {
+        this.mainNavList.toggleClass('main-nav__list--open')
+    },
+
+    fixedNav: function() {
+        if ( $(window).scrollTop() > this.mainNavOffsetTop) {
+            this.$el.addClass('main-nav--fixed');
+        } else {
+            this.$el.removeClass('main-nav--fixed');
+        }
+    }
+};
+
+App.Control.install(MainNavView);
+var VisitedPages = {
+	el: '.js-visited-pages',
+	name: 'VisitedPages',
+
+	initialize: function() {
+		this.mainSlider = $('.main-slider');
+		this.mainSliderOffsetTop = this.mainSlider.offset().top;
+		this.mainSliderHeight = this.mainSlider.outerHeight();
+
+		this.container = this.$el.parent('.container');
+		this.containerWidth = this.container.outerWidth();
+		this.elWidth = ($(window).width() - this.containerWidth) / 2;
+
+		this.pushPoint = this.mainSliderOffsetTop + this.mainSliderHeight;
+
+		var self = this;
+
+		this.setStickyBlockWidth();
+
+		$(window).bind('resize', function() {
+			self.elWidth = ($(window).width() - self.containerWidth) / 2;
+			self.setStickyBlockWidth();
+		});
+
+		$(window).bind('scroll', function() {
+			self.stickyOnScroll();
+		});
+	},
+
+	setStickyBlockWidth: function() {
+		this.$el.css({'width': this.elWidth});
+	},
+
+	stickyOnScroll: function() {
+		if($(window).scrollTop() >= this.pushPoint) {
+			this.$el.addClass('visited-pages--fixed');
+		} else {
+			this.$el.removeClass('visited-pages--fixed');
+		}
+	}
+};
+
+App.Control.install(VisitedPages);
+var MainSlider = {
+    el: '.js-main-slider',
+    name: 'MainSlider',
+    initialize: function() {
+        this.$el.bxSlider({
+            mode: 'fade',
+            pager: false,
+            auto: true,
+        });
+    }
+};
+
+App.Control.install(MainSlider);
